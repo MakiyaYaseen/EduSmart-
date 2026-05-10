d9334135d16ce107d4865b91ede9391e5bf795c4
Makiya Yaseen <yaseenmakiya@gmail.com>
2026-05-10 16:03:56 +0200

    Fix cross-site cookies for Vercel

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -91,8 +91,8 @@ export const signUp = async (req, res) => {
         // Set cookie
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "Lax",
+            secure: true,
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -139,8 +139,8 @@ export const login = async (req, res) => {
         // Set cookie
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "Lax",
+            secure: true,
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -282,8 +282,8 @@ export const googleAuth = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "Lax",
+            secure: true,
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
