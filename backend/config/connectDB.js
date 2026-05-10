63d8f48f264fe121f5ec51ae2736fce0b9c1323c
Makiya Yaseen <yaseenmakiya@gmail.com>
2026-05-10 16:14:33 +0200

    Fix Mongoose buffering timeout for serverless Vercel

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
+        if (mongoose.connections[0].readyState) {
+            console.log("DB Already Connected");
+            return;
+        }
         await mongoose.connect(process.env.MONGODB_URL);
         console.log("DB Connected Successfully");
     }
